0b9a46d99bab77e36eaeb40652549f4939460dba
Austin Meyer <austinmeyer@austins-mbp.lan>
2021-01-05 12:35:06 -0600

    Added needed entries to the upload_docker file to allow updload to docker hub

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=austinmeyer/devops_microservice_machine_learning
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login 
+docker tag devops_microservice_machine_learning austinmeyer/devops_microservice_machine_learning
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
